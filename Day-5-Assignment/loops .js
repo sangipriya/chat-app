f2b35142fdb1bd3ea7612805f3fbd191580c7ba0
sangipriya <priyakrishnar2596@gmail.com>
2023-10-02 10:35:19 -0700

    new file

---
+++ Day-5-Assignment/loops .js
@@ -0,0 +1,8 @@
+//for loop
+
+let mark=[60,75,100,25,60]
+
+for(let i=0;i<=mark.length;i++)
+{
+    console.log(mark[i])
+}
---
+++ Day-5-Assignment/script.js
@@ -0,0 +1,49 @@
+let myResume={
+"Basic information":{
+    Name:"Sangi",
+    age:26,
+    DOB:02-5-1996,
+    Education: "BCA",
+    Address:"Thiruninravur",
+    email:"priyakrishnar2596@gmail.com"
+},
+
+"profileLink":{
+
+    github:"https://github.com/sangipriya/chat-app.git" 
+},
+
+"jobDescription":{
+
+    Company:"Ecare",
+    role:"AR Analyst",
+     project:"Medical billing Analyst",
+     Experience:"4.5 years"
+
+},
+
+"languages":{
+
+Languages:"english"
+},
+
+"Skills":{
+
+    Languages : "html",
+    Framework:"VS code"
+},
+
+"hobbies":{
+    hobbies:"traveling"
+}
+
+
+};
+console.log(myResume);
+ 
+
+
+
+
+
+
