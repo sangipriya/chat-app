a178dc7f2ecc7ab5764ae88abfdfb60ee10e7b7d
sangipriya <priyakrishnar2596@gmail.com>
2023-10-07 07:40:50 -0700

    new file

---
+++ Day-4-Assignment/OddNumbers.js
@@ -0,0 +1,33 @@
+anonymous function
+
+
+
+let my =function (a){
+    let arr=[]
+for (let i=0;i<=a.length;i++){
+
+        if(a[i]%2!=0){
+ 
+           arr.push(a[i])
+console.log(arr);
+        }
+        
+    }
+}
+
+my([1,2,3,4,5]);
+
+IIFE function(immediate invoke function)
+(function (a){
+    
+    for (let i=0;i<=a.length;i++){
+        let arr=[]
+            if(a[i]%2!=0){
+                
+    arr.push(a[i])
+                console.log(arr);
+            }
+            
+        }
+    })([1,2,3,4,5])
+
